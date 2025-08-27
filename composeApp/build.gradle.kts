58ebd76096171352e740ac016f622f957cc65f2f
Alaksion <lbeurmann.dev@gmail.com>
2025-08-27 15:28:49 -0300

    feat: add missing compose desktop configuration to sample app

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,19 @@ kotlin {
         iosMain.dependencies {
             implementation(libs.mokoPermissions)
         }
+
+        val desktopMain by getting {
+            dependencies {
+                implementation("com.itextpdf:itext7-core:7.0.2")
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "main.kt"
     }
 }
 
