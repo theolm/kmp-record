0e924a472ebff7f065cb556473d31b42fce9c416
Ali Basoglu <alibasoglu1911@gmail.com>
2025-11-06 16:17:08 +0300

    chore: add documentation

--- record-core/src/commonMain/kotlin/dev/theolm/record/config/RecordConfig.kt
+++ record-core/src/commonMain/kotlin/dev/theolm/record/config/RecordConfig.kt
@@ -2,6 +2,14 @@ package dev.theolm.record.config
 
 import dev.theolm.record.VolumeCallback
 
+/**
+ * Configuration for audio recording.
+ *
+ * @param volumeCallback Optional callback for real-time volume monitoring.
+ *        **Note: Volume callback only works with WAV + PCM_16BIT format.**
+ *        For MPEG_4/AAC, the callback will not be invoked as compressed formats
+ *        don't provide direct access to audio samples.
+ */
 public data class RecordConfig(
     val outputLocation: OutputLocation = OutputLocation.Cache,
     val outputFormat: OutputFormat = OutputFormat.MPEG_4,
