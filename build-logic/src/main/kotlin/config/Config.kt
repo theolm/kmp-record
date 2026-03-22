e19a87d4ca4863578cb4a701b64c0e566deffb8b
Theodoro Mota <theolm.mota@gmail.com>
2026-03-22 20:40:11 -0300

    Updating libs

--- build-logic/src/main/kotlin/config/Config.kt
+++ build-logic/src/main/kotlin/config/Config.kt
@@ -4,9 +4,9 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val applicationId = "dev.theolm.record"
-    const val minSdk = 22
+    const val minSdk = 23
     const val targetSdk = 35
-    const val compileSdk = 35
+    const val compileSdk = 36
     const val versionCode = 1
     const val versionName = "1.0.0"
     const val packageVersion = versionName
